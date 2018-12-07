dbc754e0ae34387cf38d986d690271a35653caf4
gorouter <cloudaple@gmail.com>
2018-12-07 21:08:42 +0800

    Update and rename crontask.sh to readme.md

--- backup/task/crontask.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu
-
-rm -f /home/ubuntu/lnmpbak/site/lnmpsite-backup.tar.gz
-tar zcvf lnmpsite-backup.tar.gz ./lnmpsite/
-chown ubuntu:ubuntu lnmpsite-backup.tar.gz
-mv lnmpsite-backup.tar.gz /home/ubuntu/lnmpbak/site/
