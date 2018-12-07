8bf88c83fc4c54b241a70e3722a9b2d5c379b72a
gorouter <cloudaple@gmail.com>
2018-12-07 22:30:13 +0800

    Update crontask.sh

--- backup/crontask.sh
+++ backup/crontask.sh
@@ -3,3 +3,4 @@
 [[ -f /home/backup/lnmpsite-backup.tar.gz ]] && rm -f /home/backup/lnmpsite-backup.tar.gz
 tar zcvf /home/backup/lnmpsite-backup.tar.gz /home/site/
 chown 1000:1000 /home/backup/lnmpsite-backup.tar.gz
+
