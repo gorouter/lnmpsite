6be202ea1f0dcdb4ee4acd882f4b86db6f991ced
gorouter <cloudaple@gmail.com>
2018-12-07 21:55:05 +0800

    Update crontask.sh

--- backup/crontask.sh
+++ backup/crontask.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-[[ -f /home/lnmpsite/backup/site/lnmpsite-backup.tar.gz ]] && rm -f /home/lnmpsite/backup/site/lnmpsite-backup.tar.gz
-tar zcvf /tmp/lnmpsite-backup.tar.gz /home/lnmpsite/
-chown 1000:1000 lnmpsite-backup.tar.gz
-mv /tmp/lnmpsite-backup.tar.gz /home/lnmpsite/backup/site/
+[[ -f /home/site/backup/site/lnmpsite-backup.tar.gz ]] && rm -f /home/site/backup/site/lnmpsite-backup.tar.gz
+tar zcvf /home/site/backup/site/lnmpsite-backup.tar.gz /home/site/
+chown 1000:1000 /home/site/backup/site/lnmpsite-backup.tar.gz
 
