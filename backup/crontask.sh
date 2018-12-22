1365230160f16564321503d84780f38091ab660b
gorouter <cloudaple@gmail.com>
2018-12-22 21:46:59 +0800

    Update crontask.sh

--- backup/crontask.sh
+++ backup/crontask.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#按自定义任务创建备份文档
 [[ -f /home/backup/lnmpsite-backup.tar.gz ]] && rm -f /home/backup/lnmpsite-backup.tar.gz
 tar zcvf /home/backup/lnmpsite-backup.tar.gz /home/site/
 chown 1000:1000 /home/backup/lnmpsite-backup.tar.gz
